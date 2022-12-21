fa860e73098b85e1d5959851fe0e8e12029f1095
oolaps <121137118+oolaps@users.noreply.github.com>
2022-12-22 01:44:29 +0200

    Update bf-darkmode.js

--- bf-darkmode.js
+++ bf-darkmode.js
@@ -55,7 +55,9 @@ document.addEventListener('DOMContentLoaded', () => {
       document.querySelectorAll('.p').forEach(target => target.classList.add('is-dark'));
       document.querySelectorAll('.button_menu').forEach(target => target.classList.add('is-dark'));
       document.querySelectorAll('.image-caption').forEach(target => target.classList.add('is-dark'));
-      document.querySelectorAll('.toc').forEach(target => target.classList.add('is-dark'));              
+      document.querySelectorAll('.toc').forEach(target => target.classList.add('is-dark'));
+      document.querySelectorAll('.container-large .is-bonus-section').forEach(target => target.classList.add('is-dark'));
+      document.querySelectorAll('.border-bottom').forEach(target => target.classList.add('is-dark'));
       }
       else{ 
       document.querySelectorAll('.body').forEach(target => target.classList.remove('is-dark'));
@@ -110,7 +112,9 @@ document.addEventListener('DOMContentLoaded', () => {
       document.querySelectorAll('.p').forEach(target => target.classList.remove('is-dark')); 
       document.querySelectorAll('.button_menu').forEach(target => target.classList.remove('is-dark'));
       document.querySelectorAll('.image-caption').forEach(target => target.classList.remove('is-dark'));
-      document.querySelectorAll('.toc').forEach(target => target.classList.remove('is-dark'));     
+      document.querySelectorAll('.toc').forEach(target => target.classList.remove('is-dark'));
+      document.querySelectorAll('.container-large .is-bonus-section').forEach(target => target.classList.remove('is-dark'));
+      document.querySelectorAll('.border-bottom').forEach(target => target.classList.remove('is-dark'));
       } 
     });
  });
